a386cbf628fda6d909890e66097beabc52f4afd8
Menli <menliasyrowa91@gmail.com>
2026-04-29 11:34:31 +0500

    Create Theme.js

---
+++ src/constants/Theme.js
@@ -0,0 +1,19 @@
+export const COLORS = {
+  primary: '#007AFF', // Esasy gök reňk
+  secondary: '#5856D6',
+  background: '#F2F2F7',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#8E8E93',
+  text: '#1C1C1E',
+};
+
+export const SIZES = {
+  base: 8,
+  font: 14,
+  radius: 12,
+  padding: 24,
+};
+
+const theme = { COLORS, SIZES };
+export default theme;
